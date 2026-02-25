f4f836758e5c257ed07237fe2f2d26519ab88516
cpligit <cpli.philip@outlook.com>
2026-02-24 23:34:25 -0500

    Final infrastructure: Opened Port 80 for web access

--- main.tf
+++ main.tf
@@ -55,7 +55,13 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # For production, you'd use your specific IP
   }
-
+  ingress {
+    description = "HTTP from everywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
